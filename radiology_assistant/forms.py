1367ea561f8f575e3ee92db4f883bfac4dd8e6fa
engelln <engelln@go.stockton.edu>
2020-10-29 22:04:40 -0400

    Implemented Login backend

--- radiology_assistant/forms.py
+++ radiology_assistant/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from radiology_assistant.models import User
 
@@ -21,3 +21,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
--- radiology_assistant/routes.py
+++ radiology_assistant/routes.py
@@ -1,8 +1,8 @@
 from radiology_assistant import app, bcrypt, db
-from flask import render_template, redirect, url_for, flash
-from flask_login import current_user
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user
 from radiology_assistant.models import User
-from radiology_assistant.forms import RegistrationForm
+from radiology_assistant.forms import RegistrationForm, LoginForm
 
 
 @app.route("/")
@@ -32,9 +32,26 @@ def register():
     return render_template('register.html', title='Register', form=form)
     # return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password and try again.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+    # return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route("/admin")
 def admin():

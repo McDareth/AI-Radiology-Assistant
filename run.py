6ba2f3bf227f9e68da3637faa5132933bb8a246d
McDareth <McDareth@gmail.com>
2020-11-07 18:00:09 -0500

    Testing Github

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from radiology_assistant import app
-<<<<<<< HEAD
 
-# test comment test2
-=======
 # 2nd test comment
-# test comment
->>>>>>> 32e1c2ed7453757812687e166376ea4056399a5c
+# test comment test2
 if __name__ == '__main__':
     app.run(debug=True)
